0b5d0d4f66d0d6af32fad06751007bf173cf94e1
Ahraar <ahraralam2012@gmail.com>
2019-01-05 22:58:13 +0530

    menu

--- app/src/main/java/com/teammanagementapp/ankush/streamer/MainActivity.java
+++ app/src/main/java/com/teammanagementapp/ankush/streamer/MainActivity.java
@@ -1,6 +1,5 @@
 package com.teammanagementapp.ankush.streamer;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
